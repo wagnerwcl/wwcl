15e8ee948b97fb204584711514ebe542a2be5611
Wagner Leite <wagner.leite@wexinc.com>
2024-07-18 00:08:46 -0300

    GO/feat: Function example 2

--- go/functions/main.go
+++ go/functions/main.go
@@ -2,31 +2,22 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int // Alias
-
 func main() {
-	numbers := []int{1, 2, 3, 4}
-	doubled := transformNumbers(&numbers, double)
-	tripled := transformNumbers(&numbers, triple)
-
-	fmt.Println(doubled)
-	fmt.Println(tripled)
+	fact := factorial(3)
+	fmt.Println(fact)
 }
 
-func transformNumbers(numbers *[]int, transform transformFn) []int { // Using function as a input parameter.
-	dNumbers := []int{}
-
-	for _, value := range *numbers {
-		dNumbers = append(dNumbers, transform(value))
+func factorial(number int) int {
+	if number == 0 {
+		return 1
 	}
+	return number * factorial(number-1)
 
-	return dNumbers
-}
+	// result := 1
 
-func double(number int) int {
-	return number * 2
-}
+	// for i := 1; i <= number; i++ {
+	// 	result = result * i
+	// }
 
-func triple(number int) int {
-	return number * 3
+	// return result
 }
